bd5f3f71c03163960b94ca193fbc9a7e4912f39e
dickaestu <dickaestusaputra@gmail.com>
2020-09-30 08:52:06 +0700

    data table

--- public/admin_assets/js/demo/datatables-demo.js
+++ public/admin_assets/js/demo/datatables-demo.js
@@ -1,4 +1,9 @@
 // Call the dataTables jQuery plugin
-$(document).ready(function() {
-  $('#dataTable').DataTable();
+$(document).ready(function () {
+    $('#dataTable').DataTable({
+        "lengthMenu": [
+            [10, 25, 50, -1],
+            [10, 25, 50, "All"]
+        ]
+    });
 });
